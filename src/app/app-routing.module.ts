048d267eb0f0bbb39bebffa3bffa24d87c6d2010
deepankar96 <dipankar.sarmah1639@gmail.com>
2020-03-18 20:00:20 +0530

    login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ContributorLoginComponent } from './contributor-login/contributor-login.component';
 
 
 const routes: Routes = [
-  {path:"",component:HomepageComponent}
+  {path:"",component:HomepageComponent},
+  {path:"contributorLogin",component:ContributorLoginComponent}
+
 ];
 
 @NgModule({
