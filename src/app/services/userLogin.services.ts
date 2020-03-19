4ff0c87fd08b1a26d8f8ef60d911f2e850173a1c
deepankar96 <dipankar.sarmah1639@gmail.com>
2020-03-19 12:30:07 +0530

    user login

---
+++ src/app/services/userLogin.services.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+
+@Injectable({providedIn:'root'})
+
+export class UserLoginService{
+    private userId;
+
+    constructor(private router:Router){}
+
+    login(userId:string){
+        this.userId = userId
+        this.saveToken()
+        this.router.navigate([''])
+    }
+
+    getUserId(){
+        this.userId = localStorage.getItem("userId")
+        return this.userId
+    }
+
+    logout(){
+        this.userId = null
+        this.removeToken()
+        this.router.navigate(['userLogin'])
+    }
+
+    private saveToken(){
+        localStorage.setItem("userId",this.userId);
+        }
+
+    private removeToken(){
+        localStorage.removeItem("userId")
+    }
+}
