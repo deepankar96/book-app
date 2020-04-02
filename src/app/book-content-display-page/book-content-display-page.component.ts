f72c67805f9e16092f30d3a487189521a6a3b7ca
deepankar96 <dipankar.sarmah1639@gmail.com>
2020-04-02 19:36:48 +0530

    display paragraphs

--- src/app/book-content-display-page/book-content-display-page.component.ts
+++ src/app/book-content-display-page/book-content-display-page.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { paragraph } from 'src/model';
+import { Subscription } from 'rxjs';
+import { ParagraphService } from '../services/paragraph.services';
 
 @Component({
   selector: 'app-book-content-display-page',
   templateUrl: './book-content-display-page.component.html',
   styleUrls: ['./book-content-display-page.component.css']
 })
-export class BookContentDisplayPageComponent implements OnInit {
+export class BookContentDisplayPageComponent implements OnInit,OnDestroy {
 
-  constructor() { }
+  displayBookId:string;
+  paragraphs:paragraph[] = [];
+  private paragraphSub:Subscription;
+
+  constructor(public paragraphServices:ParagraphService) { }
 
   ngOnInit(): void {
+    this.displayBookId = localStorage.getItem('displaybookId')
+    console.log(this.displayBookId)
+    this.paragraphServices.getParagraphs(this.displayBookId)
+    this.paragraphSub = this.paragraphServices.getparagraphsListstner().subscribe(
+      (paragraphs:paragraph[])=>{
+        this.paragraphs = paragraphs
+      }
+    );
   }
-
+  ngOnDestroy(){
+    this.paragraphSub.unsubscribe()
+  }
 }
