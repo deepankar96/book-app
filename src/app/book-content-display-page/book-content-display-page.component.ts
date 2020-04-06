e5156a61f5dc3e2a37c401e1d3bef158cde9693d
deepankar96 <dipankar.sarmah1639@gmail.com>
2020-04-06 13:39:16 +0530

    book count

--- src/app/book-content-display-page/book-content-display-page.component.ts
+++ src/app/book-content-display-page/book-content-display-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { paragraph } from 'src/model';
 import { Subscription } from 'rxjs';
 import { ParagraphService } from '../services/paragraph.services';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-book-content-display-page',
@@ -13,11 +14,16 @@ export class BookContentDisplayPageComponent implements OnInit,OnDestroy {
   displayBookId:string;
   paragraphs:paragraph[] = [];
   private paragraphSub:Subscription;
+  urlToIncrementViewCount:string = 'http://localhost:3000/api/updateViewCount';
 
-  constructor(public paragraphServices:ParagraphService) { }
+  constructor(public paragraphServices:ParagraphService,private http:HttpClient) { }
 
   ngOnInit(): void {
     this.displayBookId = localStorage.getItem('displaybookId')
+    const sendingData = {
+      bookId:this.displayBookId
+    }
+    this.http.post(this.urlToIncrementViewCount,sendingData).subscribe();
     this.paragraphServices.getParagraphs(this.displayBookId)
     this.paragraphSub = this.paragraphServices.getparagraphsListstner().subscribe(
       (paragraphs:paragraph[])=>{
