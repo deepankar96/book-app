f436713c788d126faccbc1583a3fdf19c555a8f2
deepankar96 <dipankar.sarmah1639@gmail.com>
2020-05-03 13:18:33 +0530

    alert

--- src/app/super-admin-languages/super-admin-languages.component.ts
+++ src/app/super-admin-languages/super-admin-languages.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-super-admin-languages',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./super-admin-languages.component.css']
 })
 export class SuperAdminLanguagesComponent implements OnInit {
+  displayLanguageList:boolean = false;
+  urlToAddLanguage = 'http://localhost:3000/api/addLanguage'
+  urlToAddLanguageTable = 'http://localhost:3000/api/createDataTableForLanguage'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
   }
 
+
+  addLanguage(postForm:NgForm){
+    this.http.post<{message:string}>(this.urlToAddLanguage,postForm.value).subscribe(
+      (responseData)=>{
+        if(responseData.message == 'success'){
+          this.http.post<{finalMessage:string}>(this.urlToAddLanguageTable,postForm.value).subscribe(
+            (newResopnseData)=>{
+              alert(newResopnseData.finalMessage)
+            }
+          )
+        }
+      }
+    );
+  }
 }
