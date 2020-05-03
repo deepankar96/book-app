d79d9eb6a5c3461e5c2fced8e202c11251f0ef7b
deepankar96 <dipankar.sarmah1639@gmail.com>
2020-05-03 13:41:47 +0530

    language list

--- src/app/super-admin-languages/super-admin-languages.component.ts
+++ src/app/super-admin-languages/super-admin-languages.component.ts
@@ -11,12 +11,26 @@ export class SuperAdminLanguagesComponent implements OnInit {
   displayLanguageList:boolean = false;
   urlToAddLanguage = 'http://localhost:3000/api/addLanguage'
   urlToAddLanguageTable = 'http://localhost:3000/api/createDataTableForLanguage'
+  urlToGetLanguageTable = 'http://localhost:3000/api/getLanguages'
+  languages = []
 
   constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<{post}>(this.urlToGetLanguageTable).subscribe(
+      (responseData)=>{
+        this.languages = responseData.post
+      }
+    );
   }
-
+  toggleLanguageList(){
+    if(this.displayLanguageList == true){
+      this.displayLanguageList = false
+    }
+    else{
+      this.displayLanguageList = true
+    }
+  }
 
   addLanguage(postForm:NgForm){
     this.http.post<{message:string}>(this.urlToAddLanguage,postForm.value).subscribe(
